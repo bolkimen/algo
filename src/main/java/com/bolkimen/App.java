b9ca22d282968cfeef21c929246892b44f71b0bf
Sergey Dreval <sergey.dreval@auto1.com>
2019-11-27 16:26:25 +0200

    add feign client

--- src/main/java/com/bolkimen/App.java
+++ src/main/java/com/bolkimen/App.java
@@ -1,6 +1,41 @@
 package com.bolkimen;
 
 import com.bolkimen.trees.RedBlackTree;
+import feign.Feign;
+import feign.Param;
+import feign.RequestLine;
+import feign.gson.GsonDecoder;
+
+import java.util.List;
+import java.util.Objects;
+
+interface GitHub {
+    @RequestLine("GET /repos/{owner}/{repo}/contributors")
+    List<Contributor> contributors(@Param("owner") String owner, @Param("repo") String repo);
+
+    @RequestLine("POST /repos/{owner}/{repo}/issues")
+    void createIssue(Issue issue, @Param("owner") String owner, @Param("repo") String repo);
+
+    static GitHub connect() {
+        return Feign.builder()
+                .decoder(new GsonDecoder())
+                .target(GitHub.class, "https://api.github.com");
+    }
+
+}
+
+class Contributor {
+    String login;
+    int contributions;
+}
+
+class Issue {
+    String title;
+    String body;
+    List<String> assignees;
+    int milestone;
+    List<String> labels;
+}
 
 /**
  * Hello world!
@@ -10,9 +45,19 @@ public class App
 {
     public static void main( String[] args )
     {
+        Objects.equals(1, 2);
         System.out.println( "Hello World!" );
 
         RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
 
+        GitHub github = Feign.builder()
+                .decoder(new GsonDecoder())
+                .target(GitHub.class, "https://api.github.com");
+
+        // Fetch and print a list of the contributors to this library.
+        List<Contributor> contributors = github.contributors("OpenFeign", "feign");
+        for (Contributor contributor : contributors) {
+            System.out.println(contributor.login + " (" + contributor.contributions + ")");
+        }
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.bolkimen {
+    requires feign.gson;
+    requires feign.core;
+    exports com.bolkimen.trees;
+}
